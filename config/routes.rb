bfe8ba9e7464532868bd9663b4981ea45cd3c58d
ZileMaster <azivkovic10120ri@raf.rs>
2022-12-25 01:33:15 +0100

    lil new stuff(bootstrap routes and smh more)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
+  get 'home', to:'pages#home'
+  get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
 end
