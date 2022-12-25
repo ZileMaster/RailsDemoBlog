d9ecd3697c69fcf9217ae5bdc9e5a5eeb1032f0b
ZileMaster <azivkovic10120ri@raf.rs>
2022-12-25 02:32:53 +0100

    scaffolding and adding some sql in it

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ApplicationRecord
+    # validates :title, presence: true, lenght: {minimum 5, maximum 50}
+    # validates :body, presence: true, lenght: {minimum 10, maximum 1000}
+end
